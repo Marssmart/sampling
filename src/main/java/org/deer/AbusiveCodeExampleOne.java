bce80b841c9522edd352aa7359bf7d53c1491071
Restyled.io <commits@restyled.io>
2020-02-14 18:04:08 +0000

    Restyled by clang-format

--- src/main/java/org/deer/AbusiveCodeExampleOne.java
+++ src/main/java/org/deer/AbusiveCodeExampleOne.java
@@ -4,22 +4,22 @@ import java.io.ByteArrayInputStream;
 
 public class AbusiveCodeExampleOne {
 
-    public void doSomething(int input) {
-        input = input + 27;
-    }
+  public void doSomething(int input) { input = input + 27; }
 
-    public int returnUnboxed() {
-        Integer value = 4;
-        return value;
-    }
+  public int returnUnboxed() {
+    Integer value = 4;
+    return value;
+  }
 
-    public int unclosedInput() {
-        final ByteArrayInputStream in = new ByteArrayInputStream("abdc".getBytes());
-        final int read = in.read();
-        return read;
-    }
+  public int unclosedInput() {
+    final ByteArrayInputStream in = new ByteArrayInputStream("abdc".getBytes());
+    final int read = in.read();
+    return read;
+  }
 
-    public String getString() {
-        return " dsa" + "ewqdsa " + "ewqdas ewq";
-    }
+  public String getString() {
+    return " dsa"
+        + "ewqdsa "
+        + "ewqdas ewq";
+  }
 }
